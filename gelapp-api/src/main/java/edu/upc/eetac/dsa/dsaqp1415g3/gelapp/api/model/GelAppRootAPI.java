658b374bc78e84ace109161d662341eb760cdabd
ales <ales.fafe@gmail.com>
2015-04-30 12:54:59 +0200

    12:54 Fava 30/4/2015

--- gelapp-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g3/gelapp/api/model/GelAppRootAPI.java
+++ gelapp-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g3/gelapp/api/model/GelAppRootAPI.java
@@ -10,10 +10,12 @@ import org.glassfish.jersey.linking.InjectLink.Style;
 
 import edu.upc.eetac.dsa.dsaqp1415g3.gelapp.api.GelAppRootAPIResource;
 import edu.upc.eetac.dsa.dsaqp1415g3.gelapp.api.MediaType;
+import edu.upc.eetac.dsa.dsaqp1415g3.gelapp.api.SaborResource;
 
 public class GelAppRootAPI {
 	@InjectLinks({
-        @InjectLink(resource = GelAppRootAPIResource.class, style = Style.ABSOLUTE, rel = "self bookmark home", title = "GelApp Root API")})
+        @InjectLink(resource = GelAppRootAPIResource.class, style = Style.ABSOLUTE, rel = "self bookmark home", title = "GelApp Root API"),
+        @InjectLink(resource = SaborResource.class, style = Style.ABSOLUTE, rel = "sabor", title = "Get sabor", type=MediaType.GELAPP_API_SABOR)})
 	private List<Link> links;
 
 public List<Link> getLinks() {
---
+++ gelapp-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g3/gelapp/api/model/Sabor.java
@@ -0,0 +1,33 @@
+package edu.upc.eetac.dsa.dsaqp1415g3.gelapp.api.model;
+
+public class Sabor {
+	private int saborid;
+	private String name;
+	private String code_color;
+	
+	public int getSaborid() {
+		return saborid;
+	}
+	
+	public void setSaborid(int saborid) {
+		this.saborid = saborid;
+	}
+	
+	public String getCode_color() {
+		return code_color;
+	}
+	
+	public void setCode_color(String code_color) {
+		this.code_color = code_color;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
