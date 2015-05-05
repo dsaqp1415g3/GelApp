5d60099685e2b8ff360832b569183c999cc44a15
ales <ales.fafe@gmail.com>
2015-05-05 12:26:04 +0200

    Prueba obtener un sabor por id 5/5/2015

--- gelapp-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g3/gelapp/api/SaborResource.java
+++ gelapp-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g3/gelapp/api/SaborResource.java
@@ -26,41 +26,13 @@ import edu.upc.eetac.dsa.dsaqp1415g3.gelapp.api.DataSourceSPA;
 public class SaborResource {
 	private DataSource ds = DataSourceSPA.getInstance().getDataSource();
 	
-	private String GET_SABOR_BY_ID_QUERY ="select * from sabor where saborid =?";
+	private String GET_SABOR_BY_ID_QUERY ="select * from sabor where sabor_id =?";
 	@GET
 	@Path("/{saborid}")
 	@Produces(MediaType.GELAPP_API_SABOR)
 	
-	public Response getSabor(@PathParam("saborid") String saborid,
+	public Sabor getSabor(@PathParam("saborid") String saborid,
 			@Context Request request) {
-		// Create CacheControl
-		//CacheControl cc = new CacheControl();
-
-		Sabor sabor = getSaborFromDatabase(saborid);
-
-		// Calculate the ETag on last modified date of user resource
-		/*EntityTag eTag = new EntityTag(Long.toString(sabor.getLastModified()));
-
-		// Verify if it matched with etag available in http request
-		Response.ResponseBuilder rb = request.evaluatePreconditions(eTag);
-
-		// If ETag matches the rb will be non-null;
-		// Use the rb to return the response without any further processing
-		if (rb != null) {
-			return rb.cacheControl(cc).tag(eTag).build();
-		}
-
-		// If rb is null then either it is first time request; or resource is
-		// modified
-		// Get the updated representation and return with Etag attached to it
-		rb = Response.ok(sabor).cacheControl(cc).tag(eTag);
-
-		return rb.build();*/
-		
-		return sabor;
-	}
-	
-	private Sabor getSaborFromDatabase(String saborid) {
 		Sabor sabor = new Sabor();
 
 		Connection conn = null;
@@ -98,4 +70,43 @@ public class SaborResource {
 
 		return sabor;
 	}
+	
+	/*private Sabor getSaborFromDatabase(String saborid) {
+		Sabor sabor = new Sabor();
+
+		Connection conn = null;
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			throw new ServerErrorException("Could not connect to the database",
+					Response.Status.SERVICE_UNAVAILABLE);
+		}
+
+		PreparedStatement stmt = null;
+		try {
+			stmt = conn.prepareStatement(GET_SABOR_BY_ID_QUERY);
+			stmt.setInt(1, Integer.valueOf(saborid));
+			ResultSet rs = stmt.executeQuery();
+			if (rs.next()) {
+				sabor.setSaborid(rs.getInt("saborid"));
+				sabor.setName(rs.getString("nombre"));
+				sabor.setCode_color(rs.getString("codigo_color"));
+			} else {
+				throw new NotFoundException("There's no sting with stingid="
+						+ saborid);
+			}
+		} catch (SQLException e) {
+			throw new ServerErrorException(e.getMessage(),
+					Response.Status.INTERNAL_SERVER_ERROR);
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+			}
+		}
+
+		return sabor;
+	}*/
 }
