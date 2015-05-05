5a0a6c4ea91d38daadb41f1fb72bebc7ac592648
ales <ales.fafe@gmail.com>
2015-05-05 12:42:53 +0200

    Schema DB correcto

--- sql/GelApp-schema.sql
+++ sql/GelApp-schema.sql
@@ -4,19 +4,19 @@ create database gelappdb;
 use gelappdb;
 
 create table usuario (
-	usuario_id		int not null auto_increment primary key,	
+	usuario_id	int not null auto_increment primary key,	
 	username	varchar(20) not null,
 	userpass	varchar(32) not null	
 );
 
 create table sabor (
-	sabor_id		int not null auto_increment primary key,
+	sabor_id	int not null auto_increment primary key,
 	nombre		varchar(20) not null,
 	codigo_color	varchar(20) not null	
 );
 
 create table topping (
-	topping_id		int not null auto_increment primary key,
+	topping_id	int not null auto_increment primary key,
 	nombre		varchar(20) not null,
 	codigo_color	varchar(20) not null	
 );
@@ -26,29 +26,29 @@ create table helado (
 	creation_timestamp		datetime not null default current_timestamp,
 	last_modified			timestamp default current_timestamp ON UPDATE CURRENT_TIMESTAMP,
 	capa_1_topping			int not null,
-	foreign key(capa_1_topping) references topping(id) on delete cascade,
+	foreign key(capa_1_topping) references topping(topping_id) on delete cascade,
 	capa_2_helado			int not null,
-	foreign key(capa_2_helado) references sabor(id) on delete cascade,
+	foreign key(capa_2_helado) references sabor(sabor_id) on delete cascade,
 	capa_3_topping			int not null,
-	foreign key(capa_3_topping) references topping(id) on delete cascade,
+	foreign key(capa_3_topping) references topping(topping_id) on delete cascade,
 	capa_4_helado			int not null,
-	foreign key(capa_4_helado) references sabor(id) on delete cascade,
+	foreign key(capa_4_helado) references sabor(sabor_id) on delete cascade,
 	capa_5_topping			int not null,
-	foreign key(capa_5_topping) references topping(id) on delete cascade
+	foreign key(capa_5_topping) references topping(topping_id) on delete cascade
 );
 
 create table relacion_helado_usuario (
 	id_usuario			int not null,
 	id_helado 			int not null,
-	foreign key(id_helado) references helado(id) on delete cascade,
-	foreign key(id_usuario) references usuario(id) on delete cascade,
+	foreign key(id_helado) references helado(helado_id) on delete cascade,
+	foreign key(id_usuario) references usuario(usuario_id) on delete cascade,
 	primary key (id_usuario, id_helado)
 );
 
 create table votos (
 	id_usuario			int not null,
 	id_helado 			int not null,
-	foreign key(id_helado) references helado(id) on delete cascade,
-	foreign key(id_usuario) references usuario(id) on delete cascade,
+	foreign key(id_helado) references helado(helado_id) on delete cascade,
+	foreign key(id_usuario) references usuario(usuario_id) on delete cascade,
 	primary key (id_usuario, id_helado)
 );
