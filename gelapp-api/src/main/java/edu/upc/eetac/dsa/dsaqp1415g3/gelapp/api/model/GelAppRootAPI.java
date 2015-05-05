ae440111b7126518d2938acc25a9f94626becf8d
ales <ales.fafe@gmail.com>
2015-05-05 13:50:57 +0200

    Prueba sabores funciona

--- gelapp-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g3/gelapp/api/model/GelAppRootAPI.java
+++ gelapp-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415g3/gelapp/api/model/GelAppRootAPI.java
@@ -10,12 +10,12 @@ import org.glassfish.jersey.linking.InjectLink.Style;
 
 import edu.upc.eetac.dsa.dsaqp1415g3.gelapp.api.GelAppRootAPIResource;
 import edu.upc.eetac.dsa.dsaqp1415g3.gelapp.api.MediaType;
-import edu.upc.eetac.dsa.dsaqp1415g3.gelapp.api.SaborResource;
+import edu.upc.eetac.dsa.dsaqp1415g3.gelapp.api.SaboresResource;
 
 public class GelAppRootAPI {
 	@InjectLinks({
         @InjectLink(resource = GelAppRootAPIResource.class, style = Style.ABSOLUTE, rel = "self bookmark home", title = "GelApp Root API"),
-        @InjectLink(resource = SaborResource.class, style = Style.ABSOLUTE, rel = "sabor", title = "Get sabor", type=MediaType.GELAPP_API_SABOR)})
+        @InjectLink(resource = SaboresResource.class, style = Style.ABSOLUTE, rel = "sabor", title = "Get sabor", type=MediaType.GELAPP_API_SABOR)})
 	private List<Link> links;
 
 public List<Link> getLinks() {
